9760376e82f795d9283ee0509b5de2bcc03843c6
sebanc <sebanc.xda@gmail.com>
2025-04-18 20:34:04 +0200

    Fix plasma cursor theme config name

--- modules/home/plasma.nix
+++ modules/home/plasma.nix
@@ -9,7 +9,7 @@ with lib;
       enable = mkDefault true;
       kscreenlocker.appearance.wallpaper = mkDefault "${osConfig.foxflake.customization.environment.wallpaper}";
       workspace = {
-        cursorType.theme = mkDefault "${osConfig.foxflake.customization.environment.cursor-theme}";
+        cursor.theme = mkDefault "${osConfig.foxflake.customization.environment.cursor-theme}";
         iconTheme = mkDefault "${osConfig.foxflake.customization.environment.icon-theme}";
         windowDecorations.theme = "${osConfig.foxflake.customization.environment.theme}";
         wallpaper = mkDefault "${osConfig.foxflake.customization.environment.wallpaper}";
