a194f185af8f2c9c33260aea415547603da65dfe
sebanc <sebanc.xda@gmail.com>
2025-04-18 18:45:16 +0200

    Fix icons sha256sums

--- packages/foxflake-icons/default.nix
+++ packages/foxflake-icons/default.nix
@@ -8,11 +8,11 @@ let
   current_folder = ./.;
   icon_light = fetchurl {
     url = "file://${current_folder}/foxflake-icon-light.svg";
-    sha256 = "1ea334c97c077c5ed112a15d1bafdbb61f96ba5fa32843f46b33171cc1b05172";
+    sha256 = "064e105f73b195982671920b0866f4ef1e3a73fdf02b152eb69625c4098f184c";
   };
   icon_dark = fetchurl {
     url = "file://${current_folder}/foxflake-icon-dark.svg";
-    sha256 = "064e105f73b195982671920b0866f4ef1e3a73fdf02b152eb69625c4098f184c";
+    sha256 = "8443519eb29fe90ba4d42ea0e8b30d800b2842e0668949a48fe2cd0f3372c317";
   };
 in
 
