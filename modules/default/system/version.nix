0581e51807ec0c8692e4433ea1c9237291f9ca12
sebanc <sebanc.xda@gmail.com>
2025-04-18 18:15:29 +0200

    Re-add StateVersion option

--- modules/default/system/version.nix
+++ modules/default/system/version.nix
@@ -8,6 +8,12 @@ with lib;
 
 {
 
+  options.foxflake.stateVersion = mkOption {
+    description = "Initially installed FoxFlake version.";
+    type = types.str;
+    default = config.system.nixos.release;
+  };
+
   config = {
 
     system.nixos.distroName = "FoxFlake";
