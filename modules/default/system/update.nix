556433d448daec99e20dcbd2ade6e39cef398734
sebanc <sebanc.xda@gmail.com>
2025-04-18 23:12:42 +0200

    Review flatpak update scripts v4

--- modules/default/system/update.nix
+++ modules/default/system/update.nix
@@ -32,7 +32,7 @@ with lib;
         wants = [ "network-online.target" ];
         serviceConfig = {
           Type = "oneshot";
-          ExecStart = pkgs.writeScriptBin "update-system-flatpaks" ''
+          ExecStart = pkgs.writeShellScriptBin "update-system-flatpaks" ''
             #!${pkgs.bash}
             if ${pkgs.curl}/bin/curl -L https://github.com/sebanc/foxflake > /dev/null 2>&1; then
               ${pkgs.flatpak}/bin/flatpak update --assumeyes --noninteractive --system
@@ -46,7 +46,7 @@ with lib;
         description = "Update user flatpaks";
         serviceConfig = {
           Type = "oneshot";
-          ExecStart = pkgs.writeScriptBin "update-user-flatpaks" ''
+          ExecStart = pkgs.writeShellScriptBin "update-user-flatpaks" ''
             #!${pkgs.bash}
             if ${pkgs.curl}/bin/curl -L https://github.com/sebanc/foxflake > /dev/null 2>&1; then
               ${pkgs.flatpak}/bin/flatpak update --assumeyes --noninteractive
