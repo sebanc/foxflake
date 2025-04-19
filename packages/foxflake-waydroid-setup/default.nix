d617272ade07ca4df137ae804342be4948267253
sebanc <sebanc.xda@gmail.com>
2025-04-19 11:59:40 +0200

    Test waydroid install script improvement

--- packages/foxflake-waydroid-setup/default.nix
+++ packages/foxflake-waydroid-setup/default.nix
@@ -65,6 +65,7 @@ else
 
 	if [  "''${1}" == "GAPPS" ]; then
 		sleep 5
+		env
 		${pkgs.sudo}/bin/sudo -u "''${SUDO_USER}" waydroid session init
 		echo ""
 		waydroid shell 'ANDROID_RUNTIME_ROOT=/apex/com.android.runtime ANDROID_DATA=/data ANDROID_TZDATA_ROOT=/apex/com.android.tzdata ANDROID_I18N_ROOT=/apex/com.android.i18n sqlite3 /data/data/com.google.android.gsf/databases/gservices.db "select * from main where name = \"android_id\";"'
