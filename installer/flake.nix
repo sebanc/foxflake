a91bbed51211d4b503dafeebc2dd1f0e383c3ef8
sebanc <sebanc.xda@gmail.com>
2025-04-19 12:57:45 +0200

    Test waydroid install script improvement v3

--- installer/flake.nix
+++ installer/flake.nix
@@ -48,7 +48,7 @@
               { config, ... }:
               {
                 isoImage = {
-                  grubTheme = (pkgs.sleek-grub-theme.override { withBanner = "OS Selection"; withStyle = "light"; });
+                  grubTheme = (pkgs.sleek-grub-theme.override { withBanner = "FoxFlake installer"; withStyle = "light"; });
                   volumeID = "FoxFlake-ALPHA-OMNISLASH_2";
                   includeSystemBuildDependencies = false;
                   storeContents = [ config.system.build.toplevel ];
