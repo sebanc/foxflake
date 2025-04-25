d98cc933d5d4eeda4d66f9413d221c1a94f039aa
sebanc <sebanc.xda@gmail.com>
2025-04-25 23:51:22 +0200

    Review Studio bundle

--- modules/default/packages/studio.nix
+++ modules/default/packages/studio.nix
@@ -10,25 +10,15 @@ with lib;
 
   config = mkIf (builtins.elem "studio" config.foxflake.system.bundles) {
 
-    environment.variables = if (builtins.elem "amdgpu" config.services.xserver.videoDrivers) then
-      {
-        RUSTICL_ENABLE="radeonsi";
-        ROC_ENABLE_PRE_VEGA = "1";
-      }
-    else
-      {
-      };
-
     environment.systemPackages = with pkgs; [
+      audacity
       blender
+      gimp
+      inkscape-with-extensions
+      kdePackages.kdenlive
       obs-studio
       obs-studio-plugins.obs-vkcapture
-      kdePackages.kdenlive 
-      davinci-resolve
-      gimp
-      audacity
-      freetube
-      ];
+    ];
 
   };
 
