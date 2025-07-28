47224e7e5431b224bec9536fe29e03c5bfd54e7c
sebanc <sebanc.xda@gmail.com>
2025-07-28 07:06:38 +0200

    Improve AMD OpenCL support

--- modules/default/hardware/graphics.nix
+++ modules/default/hardware/graphics.nix
@@ -26,6 +26,14 @@ with lib;
       ];
     };
 
+    # AMD OpenCL support
+    systemd.tmpfiles.rules = [
+      "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+    ];
+    environment.variables = { 
+      ROC_ENABLE_PRE_VEGA = "1";
+    };
+
   };
 
 }
--- modules/default/packages/studio.nix
+++ modules/default/packages/studio.nix
@@ -12,7 +12,7 @@ with lib;
 
     environment.systemPackages = with pkgs; [
       audacity
-      blender
+      blender-hip
       gimp
       inkscape-with-extensions
       kdePackages.kdenlive
