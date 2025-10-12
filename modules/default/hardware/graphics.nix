1240cca80f797e5676f9b05948d6bd1b774bdc5f
sebanc <sebanc.xda@gmail.com>
2025-10-12 08:31:08 +0200

    Limit dependencies related to GFX

--- modules/default/hardware/graphics.nix
+++ modules/default/hardware/graphics.nix
@@ -9,20 +9,14 @@ with lib;
       enable = mkDefault true;
       enable32Bit = mkDefault true;
       extraPackages = with pkgs; [
-        intel-gpu-tools
         intel-media-driver
         vaapiIntel
         vaapiVdpau
-        libvdpau-va-gl
-        libva
       ];
       extraPackages32 = with pkgs; [
-        intel-gpu-tools
         intel-media-driver
         vaapiIntel
         vaapiVdpau
-        libvdpau-va-gl
-        libva
       ];
     };
 
