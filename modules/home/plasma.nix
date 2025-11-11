867ebe12bdff70b6501b414b0a90a7da783dc2ff
sebanc <sebanc.xda@gmail.com>
2025-11-11 07:34:22 +0100

    Disable default shortcuts list

--- modules/home/plasma.nix
+++ modules/home/plasma.nix
@@ -14,39 +14,6 @@ with lib;
         windowDecorations.theme = osConfig.foxflake.customization.environment.theme;
         wallpaper = mkDefault osConfig.foxflake.customization.environment.wallpaper;
       };
-      panels = mkDefault [
-        {
-          widgets = [
-            {
-              name = "org.kde.plasma.kickoff";
-              config = {
-                General = {
-                  icon = mkDefault osConfig.foxflake.customization.environment.launcher-icon;
-                };
-              };
-            }
-            {
-              name = "org.kde.plasma.icontasks";
-              config = {
-                General = {
-                  launchers = if builtins.elem "standard" osConfig.foxflake.system.bundles then [
-                    "applications:firefox.desktop"
-                    "applications:org.kde.dolphin.desktop"
-                    "applications:org.kde.discover.desktop"
-                  ] else [
-                    "applications:org.kde.dolphin.desktop"
-                    "applications:org.kde.discover.desktop"
-                  ];
-                };
-              };
-            }
-            "org.kde.plasma.marginsseparator"
-            "org.kde.plasma.systemtray"
-            "org.kde.plasma.digitalclock"
-            "org.kde.plasma.showdesktop"
-          ];
-        }
-      ];
       session.sessionRestore.restoreOpenApplicationsOnLogin = mkDefault "whenSessionWasManuallySaved";
     };
 
