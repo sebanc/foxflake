f8e61f9949bdbe9f519d3e3543c743d31c312d47
sebanc <sebanc.xda@gmail.com>
2025-12-01 11:35:25 +0100

    Update flake to NixOS 25.11

--- flake.nix
+++ flake.nix
@@ -3,14 +3,14 @@
   description = "FoxFlake Stable";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plasma-manager = {
-      url = "github:pjones/plasma-manager";
+      url = "github:nix-community/plasma-manager";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
@@ -33,7 +33,7 @@
                 (final: prev: {
                   unstable = import nixpkgs-unstable {
                     inherit prev;
-                    system = prev.system;
+                    inherit system;
                     config.allowUnfree = true;
                   };
                 })
