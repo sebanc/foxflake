3f14bb51338213de655efd1f9e3735d977ad044a
sebanc <sebanc.xda@gmail.com>
2026-01-08 07:05:15 +0100

    Improve Rocm / Cuda support

--- modules/default/hardware/graphics.nix
+++ modules/default/hardware/graphics.nix
@@ -20,7 +20,7 @@ with lib;
       ];
     };
 
-    # AMD OpenCL support
+    nixpkgs.config.rocmSupport = mkDefault true;
     systemd.tmpfiles.rules = [
       "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
     ];
