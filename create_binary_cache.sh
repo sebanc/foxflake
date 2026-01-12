d2dd72c62d30c819a36fe2c09614531b58791151
sebanc <sebanc.xda@gmail.com>
2026-01-12 09:39:17 +0100

    Initial attempt to setup binary cache v28

--- create_binary_cache.sh
+++ create_binary_cache.sh
@@ -2,19 +2,25 @@
 
 set -e
 
-cat >/home/runner/work/foxflake/foxflake/upload-to-cache.sh <<'BUILD_OUTPUT'
+#cat >/home/runner/work/foxflake/foxflake/upload-to-cache.sh <<BUILD_OUTPUT
 #!/bin/sh
-
-set -eu
-set -f # disable globbing
-
-if [ ! -z $OUT_PATHS ]; then
-	export IFS=' '
-	echo "Uploading paths" $OUT_PATHS
-	exec nix copy --to "file:///home/runner/work/foxflake/foxflake/foxflake-binary-cache" $OUT_PATHS
-fi
-BUILD_OUTPUT
-chmod 0755 /home/runner/work/foxflake/foxflake/upload-to-cache.sh
+#
+#set -eu
+#set -f
+#
+#export IFS=' '
+#
+#(
+#  exec >> /tmp/nix-upload.log 2>&1
+#  
+#  echo "Uploading paths: \$OUT_PATHS"
+#  
+#  nix copy --to "file:///home/runner/work/foxflake/foxflake/foxflake-binary-cache" \$OUT_PATHS
+#) &
+#
+#exit 0
+#BUILD_OUTPUT
+#chmod 0755 /home/runner/work/foxflake/foxflake/upload-to-cache.sh
 
 cat >./flake.nix <<MAIN_FLAKE
 {
@@ -231,10 +237,22 @@ for version in "stable" "unstable"; do
 	for environment in "cosmic" "gnome" "plasma"; do
 		for nvidia in "" "-nvidia"; do
 			nix build .#nixosConfigurations.foxflake-${version}-${environment}${nvidia}.config.system.build.toplevel
+			nix copy --to file:///home/runner/work/foxflake/foxflake/foxflake-binary-cache $(nix path-info --recursive --json .#nixosConfigurations.foxflake-${version}-${environment}${nvidia}.config.system.build.toplevel | jq -r 'to_entries[] | select(.value.ultimate == true) | .key')
+			#nix-collect-garbage -d
 		done
 	done
 done
 rm /home/runner/work/foxflake/foxflake/foxflake-binary-cache.priv
+for narinfo in $(ls /home/runner/work/foxflake/foxflake/foxflake-binary-cache/*.narinfo | sed 's@.narinfo@@g' | sed 's@/home/runner/work/foxflake/foxflake/foxflake-binary-cache/@@g'); do
+	narbin=$(curl --fail --silent "https://cache.nixos.org/${narinfo}.narinfo" | grep 'URL: ' | cut -d' ' -f2)
+	if [ "${narbin}" != "" ]; then
+		rm /home/runner/work/foxflake/foxflake/foxflake-binary-cache/${narinfo}.narinfo /home/runner/work/foxflake/foxflake/foxflake-binary-cache/${narbin}
+	fi
+	narbin=$(curl --fail --silent "https://cache.nixos-cuda.org/${narinfo}.narinfo" | grep 'URL: ' | cut -d' ' -f2)
+	if [ "${narbin}" != "" ]; then
+		rm /home/runner/work/foxflake/foxflake/foxflake-binary-cache/${narinfo}.narinfo /home/runner/work/foxflake/foxflake/foxflake-binary-cache/${narbin}
+	fi
+done
 
 cp ./foxflake-stable/flake.lock /home/runner/work/foxflake/foxflake/foxflake-stable-flake.lock
 cp ./foxflake-stable-test/flake.lock /home/runner/work/foxflake/foxflake/foxflake-stable-test-flake.lock
