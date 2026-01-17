55662fc7b7ababc8f485ff7bf7d8323fd9e397b1
sebanc <sebanc.xda@gmail.com>
2026-01-17 06:28:14 +0100

    Enable CUDA for blender

--- modules/default/packages/studio.nix
+++ modules/default/packages/studio.nix
@@ -12,7 +12,7 @@ with lib;
 
     environment.systemPackages = with pkgs; [
       audacity
-      blender-hip
+      (blender.override { hipSupport = true; })
       gimp
       inkscape-with-extensions
       kdePackages.kdenlive
