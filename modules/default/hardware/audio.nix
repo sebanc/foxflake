b1adbec06589137d512c436f2d3bd3178688fcb8
sebanc <sebanc.xda@gmail.com>
2026-01-30 07:28:46 +0100

    Improved fix for audio crackling

--- modules/default/hardware/audio.nix
+++ modules/default/hardware/audio.nix
@@ -15,11 +15,15 @@ with lib;
         enable = mkDefault true;
         support32Bit = mkDefault true;
       };
+      extraConfig.pipewire."92-crackling-fix" = {
+        "context.properties" = {
+          "default.clock.rate" = 48000;
+          "default.clock.min-quantum" = 1024;
+        };
+      };
       extraConfig.pipewire-pulse."92-crackling-fix" = {
         "pulse.properties" = {
-          "pulse.min.req" = "1024/48000";
-          "pulse.min.frag" = "1024/48000";
-          "pulse.min.quantum" = "1024/48000";
+          "pulse.min.req" = "16/48000";
           "pulse.idle-timeout" = 0;
         };
       };
