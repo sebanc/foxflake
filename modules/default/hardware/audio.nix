3d08d80e25812e1bfd2ae4dc780b6f86009bb02c
sebanc <sebanc.xda@gmail.com>
2026-02-04 08:27:24 +0100

    Apply pipewire upstream defaults

--- modules/default/hardware/audio.nix
+++ modules/default/hardware/audio.nix
@@ -15,15 +15,30 @@ with lib;
         enable = mkDefault true;
         support32Bit = mkDefault true;
       };
-      extraConfig.pipewire."92-crackling-fix" = {
+      extraConfig.pipewire."92-defaults" = {
         "context.properties" = {
+          "default.clock.allowed-rates" = [ 48000 ];
           "default.clock.rate" = 48000;
-          "default.clock.min-quantum" = 1024;
+          "default.clock.quantum" = 1024;
+          "default.clock.min-quantum" = 32;
+          "default.clock.max-quantum" = 2048;
+          "default.clock.quantum-floor" = 4;
+          "default.clock.quantum-limit" = 8192;
         };
       };
-      extraConfig.pipewire-pulse."92-crackling-fix" = {
+      extraConfig.pipewire-pulse."92-defaults" = {
+        "stream.properties" = {
+          "node.latency" = "1024/48000";
+          "resample.quality" = 4;
+        };
         "pulse.properties" = {
-          "pulse.min.req" = "16/48000";
+          "pulse.default.format" = "F32";
+          "pulse.min.req" = "256/48000";
+          "pulse.default.req" = "960/48000";
+          "pulse.min.frag" = "256/48000";
+          "pulse.default.frag" = "96000/48000";
+          "pulse.default.tlength" = "96000/48000";
+          "pulse.min.quantum" = "256/48000";
           "pulse.idle-timeout" = 0;
         };
       };
