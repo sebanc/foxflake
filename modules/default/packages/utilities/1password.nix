00641d01e058c13820dbbc855bc8acf208e62126
sebanc <sebanc.xda@gmail.com>
2026-03-22 21:01:00 +0100

    Add support for 1Password application

---
+++ modules/default/packages/utilities/1password.nix
@@ -0,0 +1,17 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib;
+
+{
+
+  config = mkIf (builtins.elem "full" config.foxflake.system.applications || builtins.elem "1password" config.foxflake.system.applications) {
+
+    programs._1password-gui.enable = mkDefault true;
+
+  };
+
+}
