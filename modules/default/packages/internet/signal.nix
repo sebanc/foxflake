38515ce68f752f55d56d00340e164d77cdf9c098
sebanc <sebanc.xda@gmail.com>
2026-03-31 07:20:11 +0200

    Pin Signal and Winboat to stable channel

--- modules/default/packages/internet/signal.nix
+++ modules/default/packages/internet/signal.nix
@@ -10,7 +10,7 @@ with lib;
 
   config = mkIf (builtins.elem "full" config.foxflake.system.applications || builtins.elem "signal" config.foxflake.system.applications) {
 
-    environment.systemPackages = with pkgs; [ signal-desktop ];
+    environment.systemPackages = with pkgs; [ stable.signal-desktop ];
 
   };
 
