ea4308731ff85c69ba7710449545b5077eb0177e
sebanc <sebanc.xda@gmail.com>
2026-04-25 23:11:49 +0200

    Align overlays declarations

--- packages/foxflake-icons/default.nix
+++ packages/foxflake-icons/default.nix
@@ -5,18 +5,18 @@ let
 in
 {
   nixpkgs.overlays = [
-    (self: super: {
-      foxflake-icons = self.stdenv.mkDerivation {
+    (final: prev: {
+      foxflake-icons = prev.stdenv.mkDerivation {
         name = "foxflake-icons";
         src = ./.;
-        nativeBuildInputs = [
-          self.librsvg
+        nativeBuildInputs = with final; [
+          librsvg
         ];
         installPhase = ''
 for icon in ${icons}; do
   for size in 16 24 48 64 96 128 256 512; do
     mkdir -p $out/share/icons/hicolor/''${size}x''${size}/apps
-    ${self.librsvg}/bin/rsvg-convert ''${icon}.svg -w ''${size} -h ''${size} -f svg -o $out/share/icons/hicolor/''${size}x''${size}/apps/''${icon}.svg
+    ${final.librsvg}/bin/rsvg-convert ''${icon}.svg -w ''${size} -h ''${size} -f svg -o $out/share/icons/hicolor/''${size}x''${size}/apps/''${icon}.svg
   done
 done
         '';
