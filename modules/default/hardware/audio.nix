fa17fd9ceec8c5fb5871b2b424a969f1062922da
sebanc <sebanc.xda@gmail.com>
2026-05-13 07:13:24 +0200

    Review low latency audio settings

--- modules/default/hardware/audio.nix
+++ modules/default/hardware/audio.nix
@@ -26,19 +26,31 @@ with lib;
       extraConfig.pipewire."92-low-latency" = mkIf config.foxflake.audio.lowLatency {
         "context.properties" = {
           "default.clock.rate" = 48000;
-          "default.clock.quantum" = 64;
-          "default.clock.min-quantum" = 32;
-          "default.clock.max-quantum" = 8192;
+          "default.clock.min-quantum" = 64;
+          "default.clock.quantum" = 128;
+          "default.clock.max-quantum" = 512;
         };
       };
       extraConfig.pipewire-pulse."92-low-latency" = mkIf config.foxflake.audio.lowLatency {
-        "context.properties" = {
-          "pulse.min.req" = "32/48000";
-          "pulse.default.req" = "64/48000";
-          "pulse.max.req" = "256/48000";
-          "pulse.min.quantum" = "32/48000";
-          "pulse.max.quantum" = "256/48000";
+        "pulse.properties" = {
+          "pulse.min.req" = "64/48000";
+          "pulse.default.req" = "128/48000";
+          "pulse.max.req" = "512/48000";
+          "pulse.min.quantum" = "64/48000";
+          "pulse.max.quantum" = "512/48000";
         };
+      };
+      wireplumber.extraConfig."92-low-latency" = mkIf config.foxflake.audio.lowLatency {
+        "monitor.alsa.rules" = [{
+          matches = [
+            { "node.name" = "~alsa_output.*"; }
+            { "node.name" = "~alsa_input.*"; }
+          ];
+          actions.update-props = {
+            "api.alsa.period-size"   = 128;
+            "api.alsa.headroom"      = 2;
+          };
+        }];
       };
     };
 
