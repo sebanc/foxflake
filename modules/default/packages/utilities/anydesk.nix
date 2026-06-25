46706681ea6173c48cd9c1069ee743603f3de18c
sebanc <sebanc.xda@gmail.com>
2026-06-25 07:49:35 +0200

    Manually update Anydesk to 8.0.3

--- modules/default/packages/utilities/anydesk.nix
+++ modules/default/packages/utilities/anydesk.nix
@@ -10,7 +10,15 @@ with lib;
 
   config = mkIf (builtins.elem "full" config.foxflake.system.applications || builtins.elem "anydesk" config.foxflake.system.applications) {
 
-    environment.systemPackages = with pkgs.stable; [ anydesk ];
+    environment.systemPackages = [
+      (pkgs.stable.anydesk.overrideAttrs (old: rec {
+        version = "8.0.3";
+        src = pkgs.stable.fetchurl {
+          url = "https://download.anydesk.com/linux/anydesk-${version}-amd64.tar.gz";
+          hash = "sha256-Mjl17hh5A/pwRAi7giL1SJYlQ61O0SXX+KeH8STZ4bs=";
+        };
+      }))
+    ];
 
   };
 
