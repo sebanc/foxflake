4420d74dffdcd32e808f02dbd0d602becf6e70fa
sebanc <sebanc.xda@gmail.com>
2026-07-15 15:44:46 +0200

    Final fix for input-remapper

--- modules/default/packages/gaming/input-remapper.nix
+++ modules/default/packages/gaming/input-remapper.nix
@@ -10,18 +10,6 @@ with lib;
 
   config = mkIf (builtins.elem "full" config.foxflake.system.applications || builtins.elem "input-remapper" config.foxflake.system.applications) {
 
-    nixpkgs.overlays = [
-      (final: prev: {
-        input-remapper = prev.input-remapper.overridePythonAttrs (oldAttrs: {
-          dependencies = map (pkg:
-            if pkg.pname or "" == "setuptools"
-            then final.python3Packages.setuptools_80
-            else pkg
-          ) oldAttrs.dependencies;
-        });
-      })
-    ];
-
     services.input-remapper = {
       enable = mkDefault true;
       enableUdevRules = mkDefault true;
