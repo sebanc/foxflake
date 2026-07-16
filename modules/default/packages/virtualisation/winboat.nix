34ee165a1fc039627d5c0125959705826ec78a30
sebanc <sebanc.xda@gmail.com>
2026-07-16 20:08:59 +0200

    Allow electron insecure version specifically for Winboat

--- modules/default/packages/virtualisation/winboat.nix
+++ modules/default/packages/virtualisation/winboat.nix
@@ -10,7 +10,13 @@ with lib;
 
   config = mkIf (builtins.elem "full" config.foxflake.system.applications || builtins.elem "winboat" config.foxflake.system.applications) {
 
-    environment.systemPackages = with pkgs.unstable; [ winboat ];
+    environment.systemPackages = [
+      (pkgs.stable.winboat.override {
+        electron_40 = pkgs.stable.electron_40.overrideAttrs (old: {
+          meta = old.meta // { knownVulnerabilities = [ ]; insecure = false; };
+        });
+      })
+    ];
 
   };
 
