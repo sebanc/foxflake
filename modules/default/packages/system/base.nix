6c1aff56f62a9d642278b7ab3c64aef229c30b7f
sebanc <sebanc.xda@gmail.com>
2026-08-01 06:34:18 +0200

    Add support for appimages with DwarFS

--- modules/default/packages/system/base.nix
+++ modules/default/packages/system/base.nix
@@ -11,6 +11,7 @@ with lib;
   config = {
 
     environment.systemPackages = with pkgs; [
+      binutils
       btrfs-progs
       bzip2
       dmidecode
@@ -18,13 +19,16 @@ with lib;
       e2fsprogs
       efibootmgr
       exfatprogs
+      file
       git
       gzip
       jq
       ntfs3g
       p7zip
+      patchelf
       pciutils
       psmisc
+      squashfsTools
       unzip
       usbutils
       wget
--- modules/default/system/compatibility.nix
+++ modules/default/system/compatibility.nix
@@ -15,6 +15,27 @@ with lib;
         appimage-run-foxflake = (prev.appimage-run.override {
           extraPkgs = pkgs: config.programs.nix-ld.libraries; 
         });
+        appimageTools = prev.appimageTools // {
+          appimage-exec = prev.writeShellApplication {
+            name = "appimage-exec.sh";
+            bashOptions = [ "errexit" "pipefail" ];
+            runtimeInputs = with prev; [ binutils dwarfs gnutar hexdump pv squashfsTools ];
+            text = builtins.replaceStrings
+              [ ''unsquashfs -q -d "$out" -o "$offset" "$src"'' ]
+              [ ''
+                dwarfs=$(hexdump -s "$offset" -n 6 -v -e '1/1 "%_c"' "$src")
+                if [ "$dwarfs" == "DWARFS" ]; then
+                  echo "Detected DwarFS payload, using dwarfsextract"
+                  mkdir -p "$out"
+                  dwarfsextract -i "$src" -O "$offset" -o "$out"
+                else
+                  echo "Using default unsquashfs format"
+                  unsquashfs -q -d "$out" -o "$offset" "$src"
+                fi
+              '' ]
+              (builtins.readFile "${prev.path}/pkgs/build-support/appimage/appimage-exec.sh");
+          };
+        };
       })
     ];
 
@@ -26,7 +47,7 @@ with lib;
 
     programs.nix-ld = {
       enable = mkDefault true;
-      libraries = with pkgs; [ alsa-lib alsa-plugins at-spi2-atk at-spi2-core atk brotli bzip2 cairo cups curlFull dbus dbus-glib desktop-file-utils dotnet-runtime e2fsprogs elfutils expat ffmpeg flac fontconfig freeglut freetype fribidi fuse fuse3 gdk-pixbuf git glew glib glib-networking gmp gnutls gobject-introspection graphene gsettings-desktop-schemas gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-ugly gtk2 gtk2.dev gtk3 gtk3.dev gtk4 gtk4.dev harfbuzz hpl icu json-glib libadwaita libappindicator-gtk2 libappindicator-gtk3 libayatana-appindicator libbsd libcaca libcanberra libcap libdbusmenu libdrm libelf libepoxy libgbm libgpg-error libgcrypt libGL libGLU libice libidn2 libjpeg libmanette libnotify libogg libpciaccess libpng libpsl libpulseaudio librsvg libsamplerate libsm libsoup_3 libthai libtheora libtiff libunwind libuuid libv4l libva libvdpau libvorbis libvpx libx11 libxcb libxcb-cursor libxcb-errors libxcb-image libxcb-keysyms libxcb-render-util libxcb-util libxcb-wm libxcomposite libxcrypt libxcrypt-legacy libxcursor libxdamage libxext libxfixes libxft libxi libxinerama libxkbcommon libxml2 libxmu libxrandr libxrender libxscrnsaver libxshmfence libxt libxtst libxxf86vm mesa mesa.llvmPackages.llvm.lib mono nghttp2.lib nspr nss ocl-icd openssl p11-kit pango pcscliteWithPolkit pkcs11helper pipewire pixman procps rtmpdump rocmPackages.clr sane-backends SDL_image SDL_mixer SDL_ttf SDL2_image SDL2_mixer SDL2_ttf shared-mime-info skia speex stdenv.cc.cc sudo systemd tbb udev vulkan-loader vulkan-tools wayland webkitgtk_4_1 xz zlib zstd ];
+      libraries = with pkgs; [ alsa-lib alsa-plugins at-spi2-atk at-spi2-core atk brotli bzip2 cairo cups curlFull dbus dbus-glib desktop-file-utils dotnet-runtime dwarfs e2fsprogs elfutils expat ffmpeg file flac fontconfig freeglut freetype fribidi fuse fuse3 gdk-pixbuf git glew glib glib-networking gmp gnutls gobject-introspection graphene gsettings-desktop-schemas gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-ugly gtk2 gtk2.dev gtk3 gtk3.dev gtk4 gtk4.dev harfbuzz hpl icu jre json-glib libadwaita libappindicator-gtk2 libappindicator-gtk3 libayatana-appindicator libbsd libcaca libcanberra libcap libdbusmenu libdrm libelf libepoxy libgbm libgpg-error libgcrypt libGL libGLU libice libidn2 libjpeg libmanette libnotify libogg libpciaccess libpng libpsl libpulseaudio librsvg libsamplerate libsm libsoup_3 libthai libtheora libtiff libunwind libuuid libv4l libva libvdpau libvorbis libvpx libx11 libxcb libxcb-cursor libxcb-errors libxcb-image libxcb-keysyms libxcb-render-util libxcb-util libxcb-wm libxcomposite libxcrypt libxcrypt-legacy libxcursor libxdamage libxext libxfixes libxft libxi libxinerama libxkbcommon libxml2 libxmu libxrandr libxrender libxscrnsaver libxshmfence libxt libxtst libxxf86vm mesa mesa.llvmPackages.llvm.lib mono nghttp2.lib nspr nss ocl-icd openssl p11-kit pango patchelf pcscliteWithPolkit pkcs11helper pipewire pixman procps rtmpdump rocmPackages.clr sane-backends SDL_image SDL_mixer SDL_ttf SDL2_image SDL2_mixer SDL2_ttf shared-mime-info skia speex stdenv.cc.cc sudo systemd tbb udev vulkan-loader vulkan-tools wayland webkitgtk_4_1 xz zlib zstd ];
     };
 
     environment.systemPackages = with pkgs; [
