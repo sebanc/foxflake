1ffa4452f2dcde48dbc328ae81d12156396aba84
sebanc <sebanc.xda@gmail.com>
2026-08-13 07:19:14 +0200

    Add fastfetch, mesa-demos and vulkan-tools by default

--- modules/default/environments/environment.nix
+++ modules/default/environments/environment.nix
@@ -38,7 +38,7 @@ with lib;
 
   config = mkIf config.foxflake.environment.enable {
     
-    environment.systemPackages = with pkgs; [ iio-sensor-proxy xdg-user-dirs xdg-user-dirs-gtk foxflake-icons foxflake-wallpapers ];
+    environment.systemPackages = with pkgs; [ foxflake-icons foxflake-wallpapers iio-sensor-proxy mesa-demos vulkan-tools xdg-user-dirs xdg-user-dirs-gtk ];
 
     hardware.bluetooth.enable = mkDefault true;
     hardware.sensor.iio.enable = mkDefault true;
