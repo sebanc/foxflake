fff684c529591b00157ed6d2b7fd82a5678546ed
sebanc <sebanc.xda@gmail.com>
2026-09-26 11:30:13 +0200

    Improve standalone binaries compatibility

--- modules/default/system/compatibility.nix
+++ modules/default/system/compatibility.nix
@@ -23,7 +23,7 @@ with lib;
         buildFHSEnv = args: prev.buildFHSEnv (
           args // {
             extraBuildCommands = (args.extraBuildCommands or "") + ''
-              ln -sf libFLAC.so $out/usr/lib64/libFLAC.so.8
+              ln -sf libFLAC.so "$out"/usr/lib64/libFLAC.so.8
             '';
           }
         );
@@ -32,18 +32,21 @@ with lib;
             mainProgram = "appimage-run";
           };
         } ''
-          mkdir -p $out/bin
+          mkdir -p "$out"/bin
           cp "$(readlink -f ${prev.appimage-run.override { extraPkgs = pkgs: config.programs.nix-ld.libraries; }}/bin/appimage-run)" $out/bin/appimage-run
           chmod +w $out/bin/appimage-run
-          substituteInPlace $out/bin/appimage-run \
+          substituteInPlace "$out"/bin/appimage-run \
             --replace-fail \
               'exec "''${cmd[@]}"' \
               'if [ ! -z "$APPIMAGE_USE_NIX_LD" ] && [ "$APPIMAGE_USE_NIX_LD" -eq 1 ]; then
-                echo "APPIMAGE_USE_NIX_LD is set to 1, using Nix-ld instead of appimage-run FHSEnv"
-                exec ${final.appimageTools.appimage-exec}/bin/appimage-exec.sh "$@"
-              fi
-              exec "''${cmd[@]}"'
-          chmod +x $out/bin/appimage-run
+                 echo "APPIMAGE_USE_NIX_LD is set to 1, using Nix-ld instead of appimage-run FHSEnv"
+                 export LD_LIBRARY_PATH=""$NIX_LD_LIBRARY_PATH":"$LD_LIBRARY_PATH""
+                 export SSL_CERT_FILE="/etc/ssl/certs/ca-bundle.crt"
+                 export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+                 exec ${final.appimageTools.appimage-exec}/bin/appimage-exec.sh "$@"
+               fi
+               exec "''${cmd[@]}"'
+          chmod +x "$out"/bin/appimage-run
         '';
         appimageTools = prev.appimageTools // {
           appimage-exec = prev.writeShellApplication {
@@ -77,7 +80,7 @@ with lib;
 
     programs.nix-ld = {
       enable = mkDefault true;
-      libraries = with pkgs; [ alsa-lib alsa-plugins at-spi2-atk at-spi2-core atk brotli bzip2 cairo cups curlFull dbus dbus-glib desktop-file-utils dotnet-runtime e2fsprogs elfutils expat ffmpeg file flac fontconfig freeglut freetype fribidi fuse fuse3 gdk-pixbuf git glew glib glib-networking gmp gnutls gobject-introspection graphene gsettings-desktop-schemas gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-ugly gtk2 gtk2.dev gtk3 gtk3.dev gtk4 gtk4.dev harfbuzz hpl icu jre json-glib libadwaita libappindicator-gtk3 libayatana-appindicator libbsd libcaca libcanberra libcap libdbusmenu libdrm libelf libepoxy libffi libgbm libgpg-error libgcrypt libGL libGLU libice libidn2 libjpeg libmanette libnotify libogg libpciaccess libpng libpsl libpulseaudio librsvg libsamplerate libsecret libsm libsoup_3 libthai libtheora libtiff libunwind libuuid libv4l libva libvdpau libvorbis libvpx libx11 libxcb libxcb-cursor libxcb-errors libxcb-image libxcb-keysyms libxcb-render-util libxcb-util libxcb-wm libxcomposite libxcrypt libxcrypt-legacy libxcursor libxdamage libxext libxfixes libxft libxi libxinerama libxkbcommon libxml2 libxmu libxrandr libxrender libxscrnsaver libxshmfence libxt libxtst libxxf86vm libyaml mesa mono nghttp2.lib nspr nss ocl-icd openal openssl p11-kit pango patchelf pcscliteWithPolkit pkcs11helper pipewire pixman procps rtmpdump rocmPackages.clr rocmPackages.hiprt sane-backends SDL_image SDL_mixer SDL_ttf SDL2_image SDL2_mixer SDL2_ttf shared-mime-info skia speex sqlite stdenv.cc.cc sudo systemd tbb udev vulkan-loader wayland webkitgtk_4_1 webkitgtk_6_0 xz zlib zstd ]
+      libraries = with pkgs; [ alsa-lib alsa-plugins at-spi2-atk at-spi2-core atk brotli bzip2 cairo cups curlFull dbus dbus-glib desktop-file-utils dotnet-runtime e2fsprogs elfutils expat ffmpeg file flac fontconfig freeglut freetype fribidi fuse fuse3 gdk-pixbuf git glew glib glib-networking gmp gnutls gobject-introspection graphene gsettings-desktop-schemas gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-ugly gtk2 gtk2.dev gtk3 gtk3.dev gtk4 gtk4.dev harfbuzz hpl icu jre json-glib libadwaita libappindicator-gtk3 libayatana-appindicator libbsd libcaca libcanberra libcap libdbusmenu libdrm libelf libepoxy libffi libgbm libgpg-error libgcrypt libGL libGLU libice libidn2 libjpeg libmanette libnotify libogg libpciaccess libpng libpsl libpulseaudio librsvg libsamplerate libsecret libsm libsoup_3 libthai libtheora libtiff libunwind libuuid libv4l libva libvdpau libvorbis libvpx libx11 libxcb libxcb-cursor libxcb-errors libxcb-image libxcb-keysyms libxcb-render-util libxcb-util libxcb-wm libxcomposite libxcrypt libxcrypt-legacy libxcursor libxdamage libxext libxfixes libxft libxi libxinerama libxkbcommon libxml2 libxmu libxrandr libxrender libxscrnsaver libxshmfence libxt libxtst libxxf86vm libyaml mesa mono mtdev ncurses nghttp2.lib nspr nss ocl-icd openal openssl p11-kit pango patchelf pcscliteWithPolkit pkcs11helper pipewire pixman procps readline rocmPackages.clr rocmPackages.hiprt rtmpdump sane-backends SDL_image SDL_mixer SDL_ttf SDL2_image SDL2_mixer SDL2_ttf shared-mime-info skia speex sqlite stdenv.cc.cc sudo systemd tbb udev vulkan-loader wayland webkitgtk_4_1 webkitgtk_6_0 xz zlib zstd ]
         ++ optionals (config.foxflake.graphics.compute) (with pkgs; [ rocmPackages.hipblas rocmPackages.rocblas ])
         ++ optionals (config.foxflake.nvidia.enable) (with pkgs; [ config.hardware.nvidia.package ])
         ++ optionals (config.foxflake.nvidia.enable && config.foxflake.graphics.compute) (with pkgs; [ cudaPackages.cudnn cudaPackages.libcublas cudaPackages.libcusolver cudaPackages.libcusparse ]);
@@ -87,20 +90,29 @@ with lib;
       systemPackages = with pkgs; [
         (pkgs.writeShellScriptBin "foxflake-run" ''
           # Enable the Nvidia GPU by default if detected
-          if [ -e /dev/nvidia0 ] && [ -f /run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json ] && [ ! "$FOXFLAKE_DISABLE_NVIDIA_FIX" == "1" ]; then
-            echo "foxflake-run: NVIDIA GPU detected, giving it priority. This behavior can be reverted by setting FOXFLAKE_DISABLE_NVIDIA_FIX=1."
-            export VK_DRIVER_FILES=/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json
-            export __GLX_VENDOR_LIBRARY_NAME=nvidia
+          if [ -e /dev/nvidia0 ] && [ ! "$FOXFLAKE_DISABLE_NVIDIA_FIX" == "1" ]; then
+            if [ ! -f /run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json ] || [ ! -f /run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json ]; then
+              echo "foxflake-run: NVIDIA GPU detected but not correctly configured, falling back to default."
+            else
+              echo "foxflake-run: NVIDIA GPU detected, giving it priority. This behavior can be reverted by setting FOXFLAKE_DISABLE_NVIDIA_FIX=1."
+              export OCL_ICD_VENDORS="/run/opengl-driver/etc/OpenCL/vendors/nvidia.icd"
+              export VK_DRIVER_FILES="/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json"
+              export __GLX_VENDOR_LIBRARY_NAME="nvidia"
+            fi
           fi
 
           # Add custom environment variables when launching standalone binaries to improve Nix-ld support
           bin="$1"; shift || true
           interp="$(${pkgs.patchelf}/bin/patchelf --print-interpreter "$bin" 2>/dev/null || true)"
-          if [ ! -z "$interp" ] && ! grep -q '/nix/store' "$interp"; then
+          if [ ! -z "$interp" ] && echo "$interp" | grep -q '^/lib64/ld-linux-x86-64.so'; then
             export JAVA_HOME="${pkgs.jre}"
-            export PATH="$JAVA_HOME/bin:$PATH"
+            export LD_LIBRARY_PATH=""$(dirname "$(realpath "$bin")")"/../libs:"$(dirname "$(realpath "$bin")")"/../lib64:"$(dirname "$(realpath "$bin")")"/../lib:"$NIX_LD_LIBRARY_PATH":"$LD_LIBRARY_PATH""
+            export PATH=""$(dirname "$(realpath "$bin")")":"$JAVA_HOME"/bin:"$PATH""
+            export QT_XKB_CONFIG_ROOT="${pkgs.xkeyboard_config}/share/X11/xkb"
+            export SSL_CERT_FILE="/etc/ssl/certs/ca-bundle.crt"
+            export XDG_DATA_DIRS=""$(dirname "$(realpath "$bin")")"/../share:${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:"$XDG_DATA_DIRS""
             export XLOCALEDIR="${pkgs.libx11}/share/X11/locale"
-            export XKB_CONFIG_ROOT="${pkgs.xkeyboard_config}/etc/X11/xkb"
+            export XKB_CONFIG_ROOT="${pkgs.xkeyboard_config}/share/X11/xkb"
           fi
 
           exec "$bin" "$@"
@@ -110,8 +122,8 @@ with lib;
             export LD_LIBRARY_PATH=/usr/lib64
             export GI_TYPELIB_PATH=/usr/lib64/girepository-1.0
           else
-            export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
-            export GI_TYPELIB_PATH=$NIX_LD_LIBRARY_PATH/girepository-1.0
+            export LD_LIBRARY_PATH="$NIX_LD_LIBRARY_PATH"
+            export GI_TYPELIB_PATH=""$NIX_LD_LIBRARY_PATH"/girepository-1.0"
           fi
           exec ${pkgs.python3.withPackages (module: [ module.pygobject3 module.pyqt6 ])}/bin/python "$@"
         '')
@@ -120,8 +132,8 @@ with lib;
             export LD_LIBRARY_PATH=/usr/lib64
             export GI_TYPELIB_PATH=/usr/lib64/girepository-1.0
           else
-            export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
-            export GI_TYPELIB_PATH=$NIX_LD_LIBRARY_PATH/girepository-1.0
+            export LD_LIBRARY_PATH="$NIX_LD_LIBRARY_PATH"
+            export GI_TYPELIB_PATH=""$NIX_LD_LIBRARY_PATH"/girepository-1.0"
           fi
           exec ${pkgs.python3.withPackages (module: [ module.pygobject3 module.pyqt6 ])}/bin/python "$@"
         '')
