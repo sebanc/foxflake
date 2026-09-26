63abbccba07efee49c011333b3d558b01b29fdd0
sebanc <sebanc.xda@gmail.com>
2026-09-26 11:30:13 +0200

    Enable ntsync by default

--- modules/default/packages/gaming/base.nix
+++ modules/default/packages/gaming/base.nix
@@ -21,12 +21,15 @@ with lib;
 
   config = mkIf (builtins.elem "full" config.foxflake.system.applications || config.foxflake.environment.type == "steam" || config.foxflake.environment.type == "steamdeck" || builtins.elem "gaming" config.foxflake.system.applications || builtins.elem "faugus" config.foxflake.system.applications || builtins.elem "goverlay" config.foxflake.system.applications || builtins.elem "heroic" config.foxflake.system.applications || builtins.elem "lutris" config.foxflake.system.applications || builtins.elem "mangojuice" config.foxflake.system.applications || builtins.elem "steam" config.foxflake.system.applications) {
 
-    boot.kernel.sysctl = {
-      "kernel.split_lock_mitigate" = mkOverride 999 0;
-      "net.ipv4.tcp_slow_start_after_idle" = mkOverride 999 0;
-      "net.ipv4.tcp_fastopen" = mkOverride 999 3;
-      "vm.max_map_count" = mkOverride 999 2147483642;
-      "vm.swappiness" = mkOverride 999 10;
+    boot = {
+      kernelModules = [ "ntsync" ];
+      kernel.sysctl = {
+        "kernel.split_lock_mitigate" = mkOverride 999 0;
+        "net.ipv4.tcp_slow_start_after_idle" = mkOverride 999 0;
+        "net.ipv4.tcp_fastopen" = mkOverride 999 3;
+        "vm.max_map_count" = mkOverride 999 2147483642;
+        "vm.swappiness" = mkOverride 999 10;
+      };
     };
 
     environment = {
